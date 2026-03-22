dae316c048420fce905dea28254f66b98aa3d16a
Simran Kumari <simrankumaribtech@gmail.com>
2026-03-22 17:35:11 +0530

    updated order.jsx page

--- frontend/src/pages/Order.jsx
+++ frontend/src/pages/Order.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
 import Title from '../component/Title'
 import { shopDataContext } from '../context/ShopContext'
-import { AuthContext } from '../context/authContext'
+import { AuthContext } from '../context/AuthContext'
 import axios from 'axios'
 
 function Order() {
